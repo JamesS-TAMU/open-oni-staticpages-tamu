3b18f4b806c2a235f3661fa9994b0eb5c077b61f
Jeremy Echols <jechols@uoregon.edu>
2017-07-13 17:18:01 -0700

    Don't force a slash before pagename
    This allows for static pages with any name and at any level in the path,
    including top-level paths like http://localhost/example"

--- urls.py
+++ urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url, include
 from onisite.plugins.staticpages import views
 
 urlpatterns = [
-    url(r'^/(?P<pagename>\w+)$', views.page, name="static_pages_page"),
+    url(r'^(?P<pagename>\w+)$', views.page, name="static_pages_page"),
 ]
